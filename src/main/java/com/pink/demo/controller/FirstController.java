201924f911d0e142bf2eb9b4dd57e67300a8917f
ZikHlw <zikhlw@outlook.com>
2021-10-20 22:56:16 +0800

    第一次尝试github提交

---
+++ src/main/java/com/pink/demo/controller/FirstController.java
@@ -0,0 +1,25 @@
+package com.pink.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Scanner;
+
+/**
+ * @Author Pinknee
+ * @Date 2021/10/10 2:45
+ */
+@Controller
+@RequestMapping("/test")
+public class FirstController {
+    @RequestMapping("/index")
+    public String i(){
+        return "/index";
+    }
+
+    /*public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char sex = sc.next().charAt(0);
+        System.out.println(sex);
+    }*/
+}
